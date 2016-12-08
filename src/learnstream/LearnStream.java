161cc4026f8b1ceb1d3e1246cca92b13b1c1fbef
Aleksandr Akimov <it.akimov@gmail.com>
2016-12-08 20:31:39 +0200

    identity

--- src/learnstream/LearnStream.java
+++ src/learnstream/LearnStream.java
@@ -8,6 +8,7 @@ package learnstream;
 import static java.util.Arrays.asList;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -56,6 +57,48 @@ public class LearnStream {
                max(comp).get();
                
         System.out.println(oldestPerson);
+        
+        // надо заюзать как то
+        Map<String,Person> SeniorsByKey = mapSortedByKey(capsSeniors);
+        Map<String,Person> SeniorsByValue = mapSortedByValue(capsSeniors);
+        
+        
     }
+    
+    
+    public static <K extends Comparable<? super K>, V> Map<K,V> mapSortedByKey(Map<K,V> argMap)
+    {
+        Map<K,V> mapResult = new LinkedHashMap<>();
+        Stream<Map.Entry<K,V>> sequenticalStream = argMap.entrySet().stream();
+        
+        // comparingByKey() returns a comparetor tjat compares Map.Entry in natural order on key;
+        argMap
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByKey())
+                .forEachOrdered(c-> mapResult.put(c.getKey(), c.getValue()));
+        return mapResult;
+    }
+    
+    
+    public static <K , V extends Comparable<? super V>> Map<K,V> mapSortedByValue(Map<K,V> argMap)
+    {
+        Map<K,V> mapResult = new LinkedHashMap<>();
+        Stream<Map.Entry<K,V>> sequenticalStream = argMap.entrySet().stream();
+        
+        // comparingByKey() returns a comparetor tjat compares Map.Entry in natural order on key;
+        argMap
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue())
+                .forEachOrdered(c-> mapResult.put(c.getKey(), c.getValue()));
+        return mapResult;
+    }
+    
+    public Comparator<Person> byNameLowercase = (t1,t2)-> t1.getName().toLowerCase()
+            .compareTo(t2.getName().toLowerCase());
+    
+    public Comparator<Person> byAge = (t1,t2)-> t1.getAge()
+            .compareTo(t2.getAge());
     
 }
